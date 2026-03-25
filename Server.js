0db5f98776fc114012fbba34245c2c9daaa4a2c3
mohammad sinan <muhammedsinanac111@gmail.com>
2026-03-25 06:40:08 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://192.168.50.171:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
